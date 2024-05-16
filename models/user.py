68c1e0e82b475b72b2fde50b4980000d00897398
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-05-16 15:24:08 +0100

    Created User class

---
+++ models/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Defines User Class
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+	"""
+  	A class User.
+
+  	Attributes:
+    		email (str): The user's email.
+    		password (str): The user's password
+    		first_name (str): The first name of the user
+    		last_name (str): The last name of the user
+  	"""
+
+	email = ""
+  	password = ""
+  	first_name = ""
+  	last_name = ""
+
+
+	def __str__(self):
+    		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
