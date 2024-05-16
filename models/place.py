a93518dad5a06753aaa74276e1a6bcc308616e00
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-05-16 16:10:54 +0100

    Created Place class

---
+++ models/place.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of the Place class, which represents
+a place in the AirBnB clone project.
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+	"""
+	A class representing a place. It inherits from BaseModel and adds
+	attributes specific to a place such as city_id, name, description, etc.
+	"""
+	city_id = ""
+	user_id = ""
+	name = ""
+	description = ""
+	number_rooms = 0
+	number_bathrooms = 0
+	max_guest = 0
+	price_by_night = 0
+	latitude = 0.0
+	longitude = 0.0
+	amenity_ids = []
+
+	def __str__(self):
+		"""
+		Return a string representation of the Place object.
+		"""
+		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
